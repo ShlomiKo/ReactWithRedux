97734925c3303853516e03f17e560ff06db3ce30
Shlomi Koren <koren.shlomi1@gmail.com>
2021-09-21 11:56:35 +0300

    Complete

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import configureStore from './redux/configureStore';
+import { Provider as ReduxProvider } from 'react-redux';
 
-function Hi() {
-  return <p>Hi.</p>;
-}
+const store = configureStore();
 
-render(<Hi />, document.getElementById('app'));
+render(
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
+  document.getElementById('app')
+);
